e0a53fc056fdfbe48607e90b6cd6cccbe97a6c2f
16ale <85652487+16ale@users.noreply.github.com>
2022-06-30 20:00:39 -0300

    scripta

--- scripts.js
+++ scripts.js
@@ -14,25 +14,4 @@ var swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
-const modalOverlay = document.querySelector(".modal-overlay");
-const modal = document.querySelector(".modal");
-const cards = [...document.querySelectorAll(".cards .card")];
-
-cards.forEach(card => {
-  card.addEventListener("click", () => {
-    modal.querySelector(
-      "iframe"
-    ).src = `https://www.youtube.com/embed/${card.getAttribute("id")}`;
-    modalOverlay.classList.add("active");
-    modal.classList.add("active");
-    document.querySelector("body").style.overflow = "hidden";
-  });
-});
-
-document.querySelector(".close-modal").addEventListener("click", () => {
-  modalOverlay.classList.remove("active");
-  modal.classList.remove("active");
-  modal.querySelector("iframe").src = ``;
-  document.querySelector("body").style.overflow = "initial";
-});
 
