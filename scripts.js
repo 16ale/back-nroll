ca07cffc3f11683783903becbfdc1cbbb922dabb
16ale <85652487+16ale@users.noreply.github.com>
2022-06-30 20:06:32 -0300

    scripta

--- scripts.js
+++ scripts.js
@@ -14,4 +14,15 @@ var swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+let show = true; 
 
+const menuSection = document.querySelector(".menu-section")
+const menuToggle = menuSection.querySelector(".menu-toggle")
+
+menuToggle.addEventListener("click", () => {
+
+    document.body.style.overflow = show ? "hidden" : "initial"
+
+    menuSection.classList.toggle("on", show)
+    show = !show;
+})
